5b65b2c03173b00b49a6af5d83928aa65b1ed605
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2024-02-27 14:13:52 +0100

    Add project files.

---
+++ DebugDemo2024/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace DebugDemo2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            int result = GetSum(a, b);
+
+            Console.WriteLine(result);
+            //Console.WriteLine("Hello, World!");
+
+            int res2 = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                res2 = i; 
+            }
+              
+        }
+
+        private static int GetSum(int a, int b)
+        {
+            return a + b; 
+        }
+    }
+}
